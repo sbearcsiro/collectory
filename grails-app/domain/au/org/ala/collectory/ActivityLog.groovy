6ab62421ae248143fa86b9fc8844d477e0483e3b
markew <mark.woolston@axon.com.au>
2010-06-20 22:42:28 +0000

    add roles

--- grails-app/domain/au/org/ala/collectory/ActivityLog.groovy
+++ grails-app/domain/au/org/ala/collectory/ActivityLog.groovy
@@ -1,5 +1,7 @@
 package au.org.ala.collectory
 
+import org.codehaus.groovy.grails.plugins.springsecurity.AuthorizeTools
+
 /**
  * Records user activity such as login and editing.
  */
@@ -7,6 +9,7 @@ class ActivityLog implements Serializable {
 
     Date timestamp                              // time of the event
     String user                                 // username
+    String roles                                // space separated list of roles
     long entityId = -1                          // id of the affected record if any
     boolean contactForEntity = false            // are they a contact for the record
     boolean administratorForEntity = false      // are they an administrator of the record
@@ -17,6 +20,7 @@ class ActivityLog implements Serializable {
     static constraints = {
         timestamp(blank:false)
         user(blank:false)
+        roles()
         entityId()
         contactForEntity()
         administratorForEntity()
@@ -26,16 +30,17 @@ class ActivityLog implements Serializable {
     static void log(String user, Action action) {
         //def a = Actions.valueOf(Actions.class, action)
         //def actionText = a ? a.toString() : action
-        def al = new ActivityLog(timestamp: new Date(), user: user, action: action.toString())
+        def roles = AuthorizeTools.getPrincipalAuthorities().join(" ")
+        def al = new ActivityLog(timestamp: new Date(), user: user, roles: roles, action: action.toString())
         al.errors.each {println it}
         al.save(flush:true)
     }
 
     static void log(String user, Action action, String item) {
-        new ActivityLog(timestamp: new Date(), user: user, action: action.toString() + " " + item).save(flush:true)
+        new ActivityLog(timestamp: new Date(), user: user, roles: AuthorizeTools.getPrincipalAuthorities().join(" "), action: action.toString() + " " + item).save(flush:true)
     }
 
-    // use different param order so we catch ids being passed as a string rather than creating badly formed records
+    // use different param order so we reject ids being passed as a string rather than creating badly formed records
     static void log(String user, long entityId, Action action) {
         ProviderGroup pg = ProviderGroup.findById(entityId)
         if (!pg) {
@@ -52,7 +57,8 @@ class ActivityLog implements Serializable {
                 isAdmin = cf.isAdministrator()
             }
         }
-        new ActivityLog(timestamp: new Date(), user: user, entityId: entityId, contactForEntity:isContact,
+        new ActivityLog(timestamp: new Date(), user: user, roles: AuthorizeTools.getPrincipalAuthorities().join(" "),
+                entityId: entityId, contactForEntity:isContact,
                 administratorForEntity: isAdmin, action: action.toString()).save(flush:true)
     }
 

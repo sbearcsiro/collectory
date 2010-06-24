273164d1e84f2ab5206412aaa1096b7b2955652c
markew <mark.woolston@axon.com.au>
2010-06-24 06:06:09 +0000

    add network membership, handle concurrency

--- grails-app/controllers/au/org/ala/collectory/InstitutionController.groovy
+++ grails-app/controllers/au/org/ala/collectory/InstitutionController.groovy
@@ -1,6 +1,7 @@
 package au.org.ala.collectory
 
 import org.springframework.web.multipart.MultipartFile
+import grails.converters.JSON
 
 class InstitutionController {
 
@@ -48,7 +49,7 @@ class InstitutionController {
         }
 
         showEdit {
-            render( view: "/institution/edit")
+            render(view: "/institution/edit", model: [providerGroupInstance: flow.providerGroupInstance])
             on("done").to "done"
             on("cancel").to "cancel"
             on ("removeImage") {
@@ -74,104 +75,119 @@ class InstitutionController {
                 }
                 def providerGroupInstance = ProviderGroup.get(params.id)
                 if (providerGroupInstance) {
-                    if (params.version) {
-                        def version = params.version.toLong()
-                        if (providerGroupInstance.version > version) {
-
-                            providerGroupInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'providerGroup.label', default: 'ProviderGroup')] as Object[], "Another user has updated this ProviderGroup while you were editing")
-                            render(view: "edit", model: [providerGroupInstance: providerGroupInstance])
-                            return
-                        }
-                    }
-                    // update values
-                    // need to create an address obj if one doesn't exist
-                    if (providerGroupInstance.address == null && (params.address?.street || params.address.city)) {
-                        log.debug "creating new address"
-                        providerGroupInstance.address = new Address()
-                    }
-                    if (providerGroupInstance.address) {
-                        providerGroupInstance.properties['address.street', 'address.postBox', 'address.city', 'address.state', 'address.postcode', 'address.country'] = params
-                    }
-
-                    // lat and long are shown as blank if the value is -1, and come back as blank which will be rejected
-                    providerGroupInstance.latitude = params.latitude ? params.latitude : ProviderGroup.NO_INFO_AVAILABLE
-                    providerGroupInstance.longitude = params.longitude ? params.longitude : ProviderGroup.NO_INFO_AVAILABLE
-
-                    providerGroupInstance.properties['guid', 'name', 'acronym', 'websiteUrl', 'logoRef', 'imageRef', 'isALAPartner',
-                            'institutionType', 'state', 'email', 'phone'] = params
-
-                    // handle images
-                    MultipartFile logoFile = params.logoFile
-                    if (logoFile.size) {  // will only have size if a file was selected
-                        def filename = logoFile.getOriginalFilename()
-                        log.debug "filename=${filename}"
-                        // update filename
-                        if (providerGroupInstance.logoRef) {
-                            providerGroupInstance.logoRef.file = filename
-                        } else {
-                            providerGroupInstance.logoRef = new Image(file: filename)
-                        }
-                        // save the chosen file
-                        def mhsr = request.getFile('logoFile')
-                        if (!mhsr?.empty && mhsr.size < 1024*200) {   // limit file to 200Kb
-                            def webRootDir = servletContext.getRealPath("/")
-                            def colDir = new File(webRootDir, "images/institution")
-                            colDir.mkdirs()
-                            File f = new File(colDir, filename)
-                            log.debug "saving ${filename} to ${f.absoluteFile}"
-                            mhsr.transferTo(f)
-                            ActivityLog.log authenticateService.userDomain().username as String, Action.UPLOAD_IMAGE, filename
-                        } else {
-                            // TODO: handle error message
-                        }
-                    }
-                    if (providerGroupInstance.logoRef) {
-                        // just handle changes in the image metadata
-                        providerGroupInstance.logoRef.caption = params.logoRef?.caption
-                        providerGroupInstance.logoRef.attribution = params.logoRef?.attribution
-                        providerGroupInstance.logoRef.copyright = params.logoRef?.copyright
-                    }
-
-                    MultipartFile file = params.imageFile
-                    if (file.size) {  // will only have size if a file was selected
-                        def filename = file.getOriginalFilename()
-                        log.debug "filename=${filename}"
-                        // update filename
-                        if (providerGroupInstance.imageRef) {
-                            providerGroupInstance.imageRef.file = filename
-                        } else {
-                            providerGroupInstance.imageRef = new Image(file: filename)
-                        }
-                        // save the chosen file
-                        def mhsr = request.getFile('imageFile')
-                        if (!mhsr?.empty && mhsr.size < 1024*200) {   // limit file to 200Kb
-                            def webRootDir = servletContext.getRealPath("/")
-                            def colDir = new File(webRootDir, "images/institution")
-                            colDir.mkdirs()
-                            File f = new File(colDir, filename)
-                            log.debug "saving ${filename} to ${f.absoluteFile}"
-                            mhsr.transferTo(f)
-                        } else {
-                            // TODO: handle error message
-                        }
-                    }
-                    if (providerGroupInstance.imageRef) {
-                        // just handle changes in the image metadata
-                        providerGroupInstance.imageRef.caption = params.imageRef?.caption
-                        providerGroupInstance.imageRef.attribution = params.imageRef?.attribution
-                        providerGroupInstance.imageRef.copyright = params.imageRef?.copyright
-                    }
-
-                    // save
-                    providerGroupInstance.dateLastModified = new Date()
-                    providerGroupInstance.userLastModified = authenticateService.userDomain().username
-                    if (!providerGroupInstance.hasErrors() && providerGroupInstance.save(flush: true)) {
-                        flash.message = "${message(code: 'default.updated.message', args: [message(code: 'providerGroup.label', default: 'Institution'), providerGroupInstance.name])}"
-                        ActivityLog.log authenticateService.userDomain().username as String, providerGroupInstance.id, Action.EDIT_SAVE
-                        return success()
-                    }
-                    else {
+                    providerGroupInstance.refresh()  // this seems necessary to get the current version
+                    log.debug "db version = ${providerGroupInstance.version}"
+                    log.debug "params version = ${params.version}"
+                    if (params.version && providerGroupInstance.version > params.version.toLong()) {
+                        log.warn "Attempted to save a stale institution record - ${providerGroupInstance.name} version ${providerGroupInstance.version}"
+                        providerGroupInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
+                                [message(code: 'providerGroup.label', default: 'ProviderGroup')] as Object[],
+                                "Another user has updated this ProviderGroup while you were editing. New values have been refreshed. You will need to reapply your changes.")
+                        flow.providerGroupInstance.discard()
+                        flow.providerGroupInstance = providerGroupInstance
                         return error()
+                    } else {
+                        // update values
+                        // need to create an address obj if one doesn't exist
+                        if (providerGroupInstance.address == null && (params.address?.street || params.address.city)) {
+                            log.debug "creating new address"
+                            providerGroupInstance.address = new Address()
+                        }
+                        if (providerGroupInstance.address) {
+                            providerGroupInstance.properties['address.street', 'address.postBox', 'address.city', 'address.state', 'address.postcode', 'address.country'] = params
+                        }
+
+                        // lat and long are shown as blank if the value is -1, and come back as blank which will be rejected
+                        providerGroupInstance.latitude = params.latitude ? params.latitude : ProviderGroup.NO_INFO_AVAILABLE
+                        providerGroupInstance.longitude = params.longitude ? params.longitude : ProviderGroup.NO_INFO_AVAILABLE
+
+                        providerGroupInstance.properties['guid', 'name', 'acronym', 'websiteUrl', 'logoRef', 'imageRef', 'isALAPartner',
+                                'institutionType', 'state', 'email', 'phone'] = params
+
+                        // network membership can be returned as a String if there is a single value or a String []
+                        if (params.networkMembership) {
+                            def list
+                            if (params.networkMembership instanceof String[]) {
+                                list = params.networkMembership.toList()
+                            } else {
+                                list = new ArrayList() << params.networkMembership
+                            }
+                            providerGroupInstance.networkMembership = (list as JSON).toString();
+                        }
+
+                        // handle images
+                        MultipartFile logoFile = params.logoFile
+                        if (logoFile.size) {  // will only have size if a file was selected
+                            def filename = logoFile.getOriginalFilename()
+                            log.debug "filename=${filename}"
+                            // update filename
+                            if (providerGroupInstance.logoRef) {
+                                providerGroupInstance.logoRef.file = filename
+                            } else {
+                                providerGroupInstance.logoRef = new Image(file: filename)
+                            }
+                            // save the chosen file
+                            def mhsr = request.getFile('logoFile')
+                            if (!mhsr?.empty && mhsr.size < 1024*200) {   // limit file to 200Kb
+                                def webRootDir = servletContext.getRealPath("/")
+                                def colDir = new File(webRootDir, "images/institution")
+                                colDir.mkdirs()
+                                File f = new File(colDir, filename)
+                                log.debug "saving ${filename} to ${f.absoluteFile}"
+                                mhsr.transferTo(f)
+                                ActivityLog.log authenticateService.userDomain().username as String, Action.UPLOAD_IMAGE, filename
+                            } else {
+                                // TODO: handle error message
+                            }
+                        }
+                        if (providerGroupInstance.logoRef) {
+                            // just handle changes in the image metadata
+                            providerGroupInstance.logoRef.caption = params.logoRef?.caption
+                            providerGroupInstance.logoRef.attribution = params.logoRef?.attribution
+                            providerGroupInstance.logoRef.copyright = params.logoRef?.copyright
+                        }
+
+                        MultipartFile file = params.imageFile
+                        if (file.size) {  // will only have size if a file was selected
+                            def filename = file.getOriginalFilename()
+                            log.debug "filename=${filename}"
+                            // update filename
+                            if (providerGroupInstance.imageRef) {
+                                providerGroupInstance.imageRef.file = filename
+                            } else {
+                                providerGroupInstance.imageRef = new Image(file: filename)
+                            }
+                            // save the chosen file
+                            def mhsr = request.getFile('imageFile')
+                            if (!mhsr?.empty && mhsr.size < 1024*200) {   // limit file to 200Kb
+                                def webRootDir = servletContext.getRealPath("/")
+                                def colDir = new File(webRootDir, "images/institution")
+                                colDir.mkdirs()
+                                File f = new File(colDir, filename)
+                                log.debug "saving ${filename} to ${f.absoluteFile}"
+                                mhsr.transferTo(f)
+                            } else {
+                                // TODO: handle error message
+                            }
+                        }
+                        if (providerGroupInstance.imageRef) {
+                            // just handle changes in the image metadata
+                            providerGroupInstance.imageRef.caption = params.imageRef?.caption
+                            providerGroupInstance.imageRef.attribution = params.imageRef?.attribution
+                            providerGroupInstance.imageRef.copyright = params.imageRef?.copyright
+                        }
+
+                        // save
+                        providerGroupInstance.dateLastModified = new Date()
+                        providerGroupInstance.userLastModified = authenticateService.userDomain().username
+                        if (!providerGroupInstance.hasErrors() && providerGroupInstance.save(flush: true)) {
+                            flash.message = "${message(code: 'default.updated.message', args: [message(code: 'providerGroup.label', default: 'Institution'), providerGroupInstance.name])}"
+                            ActivityLog.log authenticateService.userDomain().username as String, providerGroupInstance.id, Action.EDIT_SAVE
+                            return success()
+                        }
+                        else {
+                            return error()
+                        }
                     }
                 } else {
                     flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'providerGroup.label', default: 'ProviderGroup'), params.id])}"
@@ -206,6 +222,7 @@ class InstitutionController {
                 if (contact) {
                     flow.providerGroupInstance.deleteFromContacts(contact)
                 }
+                flow.providerGroupInstance
             }
             on("success").to "showEdit"
         }

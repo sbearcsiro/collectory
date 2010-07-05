7af9ea1c2d61313cbd26a0b216731db9dd4d4b70
markew <mark.woolston@axon.com.au>
2010-07-05 05:09:43 +0000

    implement JSON summary

---
+++ src/groovy/au/org/ala/collectory/CollectionSummary.groovy
@@ -0,0 +1,14 @@
+package au.org.ala.collectory
+
+/**
+ * TO for the summary of a collection.
+ */
+class CollectionSummary {
+    long id
+    String name
+    String acronym
+    String shortDescription
+    String lsid
+    String institution
+    String institutionId
+}

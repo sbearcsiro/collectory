abfc2bbfa030f41f0faaf458ab741763b07f3a59
markew <mark.woolston@axon.com.au>
2010-07-12 01:29:23 +0000

    add db dialect add properties for prod db

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -15,6 +15,7 @@ environments {
 	development {
 		dataSource {
 //            logSql = "true"
+            dialect = org.hibernate.dialect.MySQL5InnoDBDialect
 			dbCreate = "update" // one of 'create', 'create-drop','update'
             url = "jdbc:mysql://localhost:3306/collectory?autoReconnect=true&connectTimeout=0"
 //			url = "jdbc:hsqldb:mem:devDB"
@@ -31,12 +32,20 @@ environments {
 	}
 	production {
 		dataSource {
+            dialect = org.hibernate.dialect.MySQL5InnoDBDialect
 			dbCreate = "update"
 			url = "jdbc:mysql://alaproddb1-cbr.vm.csiro.au:3306/collectory?autoReconnect=true&connectTimeout=0"//"jdbc:hsqldb:file:prodDb;shutdown=true"
             logSql = false
-            autoReconnect = true
-            maxActive=10
-            initialSize=5
+            properties {
+                maxActive = 50
+                maxIdle = 25
+                minIdle = 5
+                initialSize = 5
+                minEvictableIdleTimeMillis = 60000
+                timeBetweenEvictionRunsMillis = 60000
+                maxWait = 10000
+                validationQuery = "/* ping */"
+            }
 		}
 	}
 }

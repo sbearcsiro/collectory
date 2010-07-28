a80fea4d912447e4dab7d4e45529929fa1693636
markew <mark.woolston@axon.com.au>
2010-07-28 22:06:05 +0000

    make public map the default page add more readable mappings

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -5,7 +5,11 @@ class UrlMappings {
 			 // apply constraints here
 		  }
 	  }
-      "/"(view:"/index")
+      "/collection/$id?"(controller:'public', action:'show')
+      "/institution/$id?"(controller:'public', action:'showInstitution')
+      "/lookup/institution/code/$code"(controller:'lookup', action:'findInstitution')
+      "/lookup/institution/$id"(controller:'lookup', action:'findInstitution')
+      "/"(view:"/public/map")
 	  "500"(view:'/error')
 	}
 }

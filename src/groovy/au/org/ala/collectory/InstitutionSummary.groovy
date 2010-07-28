686ca625c8c292c1fa8b7979b938c90456dabf7f
markew <mark.woolston@axon.com.au>
2010-07-28 22:04:58 +0000

    refactor summaries

---
+++ src/groovy/au/org/ala/collectory/InstitutionSummary.groovy
@@ -0,0 +1,11 @@
+package au.org.ala.collectory
+
+/**
+ * Created by markew
+ * Date: Jul 28, 2010
+ * Time: 3:32:16 PM
+ */
+class InstitutionSummary extends ProviderGroupSummary {
+    List derivedInstCodes
+    List collections
+}

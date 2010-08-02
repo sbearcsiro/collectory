70d1a92befe0b402fecdd4a16ae7d01ce3a76a9f
markew <mark.woolston@axon.com.au>
2010-08-02 02:01:24 +0000

    add export action

--- grails-app/controllers/au/org/ala/collectory/AdminController.groovy
+++ grails-app/controllers/au/org/ala/collectory/AdminController.groovy
@@ -1,5 +1,7 @@
 package au.org.ala.collectory
 
+import grails.converters.JSON
+
 class AdminController {
 
     def authenticateService, dataLoaderService
@@ -19,5 +21,39 @@ class AdminController {
         dataLoaderService.loadAmrrnData()
         ActivityLog.log authenticateService.userDomain().username, Action.DATA_LOAD
         redirect(url: "http://localhost:8080/Collectory")  //action: "list")
+    }
+
+    /**
+     * Export all tables as JSON
+     */
+    def export = {
+        println params.table
+        if (params.table) {
+            switch (params.table) {
+                case 'contact': render Contact.list() as JSON; break
+                case 'contactFor': render ContactFor.list() as JSON; break
+                case 'providerGroup': render ProviderGroup.list() as JSON; break
+                case 'collection': render ProviderGroup.findAllByGroupType(ProviderGroup.GROUP_TYPE_COLLECTION) as JSON; break
+                case 'institution': render ProviderGroup.findAllByGroupType(ProviderGroup.GROUP_TYPE_INSTITUTION) as JSON; break
+                case 'collectionScope': render CollectionScope.list() as JSON; break
+                case 'infoSource': render InfoSource.list() as JSON; break
+                case 'providerCode': render ProviderCode.list() as JSON; break
+                case 'providerMap': render ProviderMap.list() as JSON; break
+                default:
+                    def error = ['error','no such table']
+                    render error as JSON
+            }
+        } else {
+            def result = [
+                    contact: Contact.list(),
+                    contactFor: ContactFor.list(),
+                    providerGroup: ProviderGroup.list(),
+                    collectionScope: CollectionScope.list(),
+                    infoSource: InfoSource.list(),
+                    providerCode: ProviderCode.list(),
+                    providerMap: ProviderMap.list()
+            ]
+            render result as JSON
+        }
     }
 }

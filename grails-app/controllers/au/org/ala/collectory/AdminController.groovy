8e55cee91032e7a81a2206a14d2ef17f0f9f7e3d
markew <mark.woolston@axon.com.au>
2010-08-09 06:33:46 +0000

    refactor main entities to use inheritance hide auth til new security system

--- grails-app/controllers/au/org/ala/collectory/AdminController.groovy
+++ grails-app/controllers/au/org/ala/collectory/AdminController.groovy
@@ -4,7 +4,7 @@ import grails.converters.JSON
 
 class AdminController {
 
-    def authenticateService, dataLoaderService
+    def dataLoaderService
 
     def index = { }
 
@@ -12,14 +12,14 @@ class AdminController {
         boolean override = params.override ? params.override : false
         log.info ">>${authenticateService.userDomain().username} loading supplimentary data"
         dataLoaderService.loadSupplementaryData("/data/collectory/bootstrap/sup.json", override, authenticateService.userDomain().username)
-        ActivityLog.log authenticateService.userDomain().username, Action.DATA_LOAD
+//        ActivityLog.log authenticateService.userDomain().username, Action.DATA_LOAD
         redirect(url: "http://localhost:8080/Collectory")  //action: "list")
     }
 
     def loadAmrrnData = {
         log.info ">>${authenticateService.userDomain().username} loading amrrn data"
         dataLoaderService.loadAmrrnData()
-        ActivityLog.log authenticateService.userDomain().username, Action.DATA_LOAD
+//        ActivityLog.log authenticateService.userDomain().username, Action.DATA_LOAD
         redirect(url: "http://localhost:8080/Collectory")  //action: "list")
     }
 
@@ -27,16 +27,13 @@ class AdminController {
      * Export all tables as JSON
      */
     def export = {
-        println params.table
         if (params.table) {
             switch (params.table) {
                 case 'contact': render Contact.list() as JSON; break
                 case 'contactFor': render ContactFor.list() as JSON; break
-                case 'providerGroup': render ProviderGroup.list() as JSON; break
-                case 'collection': render ProviderGroup.findAllByGroupType(ProviderGroup.GROUP_TYPE_COLLECTION) as JSON; break
-                case 'institution': render ProviderGroup.findAllByGroupType(ProviderGroup.GROUP_TYPE_INSTITUTION) as JSON; break
-                case 'collectionScope': render CollectionScope.list() as JSON; break
-                case 'infoSource': render InfoSource.list() as JSON; break
+                case 'collection': render Collection.list() as JSON; break
+                case 'institution': render Institution.list() as JSON; break
+                //case 'infoSource': render InfoSource.list() as JSON; break
                 case 'providerCode': render ProviderCode.list() as JSON; break
                 case 'providerMap': render ProviderMap.list() as JSON; break
                 default:
@@ -47,13 +44,47 @@ class AdminController {
             def result = [
                     contact: Contact.list(),
                     contactFor: ContactFor.list(),
-                    providerGroup: ProviderGroup.list(),
-                    collectionScope: CollectionScope.list(),
-                    infoSource: InfoSource.list(),
+                    collection: Collection.list(),
+                    institution: Institution.list(),
+                    //infoSource: InfoSource.list(),
                     providerCode: ProviderCode.list(),
                     providerMap: ProviderMap.list()
             ]
             render result as JSON
         }
+    }
+
+    def importJson = {
+        dataLoaderService.importJson()
+        // some tests
+        if (Institution.findByName("Tasmanian Museum and Art Gallery")?.getContacts()?.size() == 1) {
+            render "loaded ok"
+        } else {
+            render "failed to load correctly"
+        }
+
+    }
+
+    def testImport = {
+        String response = ""
+
+        def inst1 = Institution.findByName("Tasmanian Museum and Art Gallery")
+        response += inst1.name + "<br>"
+        def fors1 = inst1.getContacts()
+        fors1.each {response += '_' + it.contact + "<br>"}
+
+        def col1 = Collection.findByName("Australian National Herbarium")
+        response += col1.name + "<br>"
+        def fors2 = col1.getContacts()
+        fors2.each {response += '_' + it.contact + "<br>"}
+
+        def inst2 = Institution.findByAcronym('CSIRO')
+        response += inst2.name + "<br>"
+        def children = inst2?.getCollections()
+        children?.each {
+            response += '_' + it.name + "<br>"
+        }
+
+        render response
     }
 }

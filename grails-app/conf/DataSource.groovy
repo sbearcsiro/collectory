fff907586e4298722e6b60d35d1a4ffcc179c153
markew <mark.woolston@axon.com.au>
2010-08-16 22:46:22 +0000

    introduce testserver environment (for builds to test server) temporarily protect prod database

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -17,7 +17,7 @@ environments {
 //            logSql = "true"
             dialect = org.hibernate.dialect.MySQL5InnoDBDialect
 			dbCreate = "update" // one of 'create', 'create-drop','update'
-            url = "jdbc:mysql://localhost:3306/collectory?autoReconnect=true&connectTimeout=0"
+            url = "jdbc:mysql://localhost:3306/coll?autoReconnect=true&connectTimeout=0"
 //			url = "jdbc:hsqldb:mem:devDB"
 		}
 	}
@@ -30,11 +30,30 @@ environments {
             password = ""
 		}
 	}
+	testserver {
+        dataSource {
+            dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+            dbCreate = "update"
+            url = "jdbc:mysql://alatstweb1-cbr.vm.csiro.au:3306/collectory?autoReconnect=true&connectTimeout=0"
+            logSql = false
+            properties {
+                maxActive = 50
+                maxIdle = 25
+                minIdle = 5
+                initialSize = 5
+                minEvictableIdleTimeMillis = 60000
+                timeBetweenEvictionRunsMillis = 60000
+                maxWait = 10000
+                validationQuery = "/* ping */"
+            }
+        }
+	}
 	production {
 		dataSource {
             dialect = org.hibernate.dialect.MySQL5InnoDBDialect
 			dbCreate = "update"
-			url = "jdbc:mysql://alaproddb1-cbr.vm.csiro.au:3306/collectory?autoReconnect=true&connectTimeout=0"//"jdbc:hsqldb:file:prodDb;shutdown=true"
+			url = "jdbc:hsqldb:file:prodDb;shutdown=true"
+			//url = "jdbc:mysql://alaproddb1-cbr.vm.csiro.au:3306/collectory?autoReconnect=true&connectTimeout=0"//"jdbc:hsqldb:file:prodDb;shutdown=true"
             logSql = false
             properties {
                 maxActive = 50

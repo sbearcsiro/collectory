2dcdc149dd3ee4e73b4ae55ce5137f961a69b839
markew <mark.woolston@axon.com.au>
2010-09-01 23:29:13 +0000

    add contextPath to CAS config

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -5,6 +5,8 @@ eventWebXmlStart = {
         def tmpWebXml = "${projectWorkDir}/web.xml.tmp"
         ant.replace(file: tmpWebXml, token: "@security.cas.serverName@", value: ConfigurationHolder.config.security.cas.serverName)
         println "Injecting CAS Security Configuration: serverName = ${ConfigurationHolder.config.security.cas.serverName}"
+        ant.replace(file: tmpWebXml, token: "@security.cas.contextPath@", value: ConfigurationHolder.config.security.cas.contextPath)
+        println "Injecting CAS Security Configuration: contextPath = ${ConfigurationHolder.config.security.cas.contextPath}"
         ant.replace(file: tmpWebXml, token: "/substitute-me", value: ConfigurationHolder.config.security.cas.urlPattern)
         println "Injecting CAS Security Configuration: url pattern = ${ConfigurationHolder.config.security.cas.urlPattern}"
     }

8676176f546ef2f2294b990bb28a32e46a3eb116
markew <mark.woolston@axon.com.au>
2010-09-14 04:49:43 +0000

    use custom marshaller to output uid for domain class associations

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -2,14 +2,17 @@ import au.org.ala.collectory.ProviderGroup
 import au.org.ala.collectory.Image
 import au.org.ala.collectory.Contact
 import grails.util.GrailsUtil
+import grails.converters.JSON
+import au.org.ala.custom.marshalling.DomainClassWithUidMarshaller
 
 class BootStrap {
 
     def dataLoaderService
     def authenticateService
 
-
     def init = { servletContext ->
+        // custom marshaller to put UID into the JSON representation of associations
+        JSON.registerObjectMarshaller( new DomainClassWithUidMarshaller(false), 2)
     }
 
     def destroy = {

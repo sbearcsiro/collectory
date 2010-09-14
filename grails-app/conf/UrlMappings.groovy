c7781b06c96106839de628db606a596158cae659
markew <mark.woolston@axon.com.au>
2010-09-14 05:01:47 +0000

    add RESTful data services

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,6 +1,6 @@
 class UrlMappings {
     static mappings = {
-      "/$controller/$action?/$id?"{
+      name standard: "/$controller/$action?/$id?"{
 	      constraints {
 			 // apply constraints here
 		  }
@@ -9,6 +9,21 @@ class UrlMappings {
       "/pub/institution/$id?"(controller:'public', action:'showInstitution')
       "/lookup/inst/$inst/coll/$coll"(controller:'lookup',action:'collection')
       "/admin/export/$table" (controller:'admin',action:'export')
+      "/co/$uid?" (controller:'data') {
+          action = [GET:'getCollection', PUT:'saveCollection', DELETE:'delete', POST:'saveCollection']
+       }
+      "/in/$uid?" (controller:'data') {
+          action = [GET:'getInstitution', PUT:'saveInstitution', DELETE:'delete', POST:'saveInstitution']
+       }
+      "/dp/$uid?" (controller:'data') {
+          action = [GET:'getDataProvider', PUT:'saveDataProvider', DELETE:'delete', POST:'saveDataProvider']
+       }
+      "/dr/$uid?" (controller:'data') {
+          action = [GET:'getDataResource', PUT:'saveDataResource', DELETE:'delete', POST:'saveDataResource']
+       }
+      "/dh/$uid?" (controller:'data') {
+          action = [GET:'getDataHub', PUT:'saveDataHub', DELETE:'delete', POST:'saveDataHub']
+       }
       "/"(controller:'public', action:'map')
 	  "500"(view:'/error')
 	}

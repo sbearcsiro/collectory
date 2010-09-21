0f40bdec0ff8e0f4ed5142d45f6af678bdca32e3
markew <mark.woolston@axon.com.au>
2010-09-21 00:27:40 +0000

    add institution to summary

--- src/groovy/au/org/ala/collectory/DataResourceSummary.groovy
+++ src/groovy/au/org/ala/collectory/DataResourceSummary.groovy
@@ -4,5 +4,7 @@ class DataResourceSummary extends ProviderGroupSummary {
     String dataProvider
     String dataProviderId
     String dataProviderUid
+    String institution
+    String institutionUid
     String displayName
 }

4693d567ddc4be04822fb97bf78df1b231eb61cb
markew <mark.woolston@axon.com.au>
2010-09-30 04:22:31 +0000

    evictor bean does not seem to be required (for maintaining db connections)

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,16 +1,4 @@
-import org.apache.commons.dbcp.BasicDataSource
-
 // Place your Spring DSL code here
 beans = {
-    datasource(BasicDataSource) {
-        // run evictor every 30 minutes and evict any connections older than 30 minutes.
-        minEvictableIdleTimeMillis=1800000
-        timeBetweenEvictionRunsMillis=1800000
-        numTestsPerEvictionRun=3
-        // test the connection while it's idle
-        testOnBorrow=true
-        testWhileIdle=true
-        testOnReturn=true
-        validationQuery="SELECT 1"
-    }
+    
 }

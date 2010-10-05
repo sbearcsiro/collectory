6579f788fb15cc3637174cb8512758e8fd2b75b1
markew <mark.woolston@axon.com.au>
2010-10-05 05:21:27 +0000

    upgrade to grails-1.3.5

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -68,6 +68,9 @@ grails.mime.types = [ html: ['text/html','application/xhtml+xml'],
                       form: 'application/x-www-form-urlencoded',
                       multipartForm: 'multipart/form-data'
                     ]
+// URL Mapping Cache Max Size, defaults to 5000
+//grails.urlmapping.cache.maxsize = 1000
+
 // The default codec used to encode data with ${}
 grails.views.default.codec="html" // none, html, base64
 grails.views.gsp.encoding="UTF-8"

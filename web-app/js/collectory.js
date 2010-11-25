8b7867d9a75c38b52261080ec83292c77808104a
markew <mark.woolston@axon.com.au>
2010-11-25 05:11:17 +0000

    extract common script for charts from views

--- web-app/js/collectory.js
+++ web-app/js/collectory.js
@@ -1,3 +1,228 @@
+/*------------------------- TAXA BREAKDOWN CHARTS ------------------------------*/
+
+var instanceUid;
+var taxaThreshold;
+// the server base url
+var baseUrl;
+var biocacheUrl = "http://biocache.ala.org.au/";  // TODO: inject this
+var taxaChartOptions = {
+    width: 400,
+    height: 400,
+    chartArea: {left:0, top:30, width:"90%", height: "75%"},
+    is3D: false,
+    titleTextStyle: {color: "#555", fontName: 'Arial', fontSize: 15},
+    sliceVisibilityThreshold: 0,
+    legend: "left"
+};
+/************************************************************\
+* Ajax request for taxa breakdown
+\************************************************************/
+function loadTaxonChart(serverUrl, uid, threshold, options) {
+  instanceUid = uid;
+  taxaThreshold = threshold;
+  baseUrl = serverUrl;
+  setChartOptions(taxaChartOptions, options);
+  var taxonUrl = baseUrl + "/public/taxonBreakdown/" + uid +
+          "?threshold=" + threshold;
+  $.get(taxonUrl, {}, taxonBreakdownRequestHandler);
+}
+/************************************************************\
+* Ajax request for rank breakdown
+\************************************************************/
+function loadTaxonChartByRank(uid, name, rank) {
+  var taxonUrl = baseUrl + "/public/rankBreakdown/" + uid +
+          "?name=" + name +
+          "&rank=" + rank;
+  $.get(taxonUrl, {}, taxonBreakdownRequestHandler);
+}
+/************************************************************\
+* Handler for taxa breakdown - hides chart if no data
+\************************************************************/
+function taxonBreakdownRequestHandler(response) {
+  if (response.error == undefined) {
+    var data = new google.visualization.DataTable(response);
+    if (data.getNumberOfRows() > 0) {
+      drawTaxonChart(data);
+    } else {
+        // no data
+        $('div#taxonChart').css("display","none");
+        $('div#taxonChartCaption').css("display","none");
+    }
+  } else {
+    // an error occurred
+    $('div#taxonChart').css("display","none");
+    $('div#taxonChartCaption').css("display","none");
+  }
+}
+/************************************************************\
+* Draw the chart
+\************************************************************/
+function drawTaxonChart(dataTable) {
+  var chart = new google.visualization.PieChart(document.getElementById('taxonChart'));
+
+  if (dataTable.getTableProperty('scope') == "all") {
+    taxaChartOptions.title = "Records by " + dataTable.getTableProperty('rank');
+  } else {
+    taxaChartOptions.title = dataTable.getTableProperty('name') + " records by " + dataTable.getTableProperty('rank')
+  }
+  google.visualization.events.addListener(chart, 'select', function() {
+    var rank = dataTable.getTableProperty('rank');
+    var name = dataTable.getValue(chart.getSelection()[0].row,0);
+    // differentiate between clicks on legend versus slices
+    if (chart.getSelection()[0].column == undefined) {
+      // clicked legend - show records
+      var scope = dataTable.getTableProperty('scope');
+      if (scope == "genus" && rank == "species") {
+        name = dataTable.getTableProperty('name') + " " + name;
+      }
+      document.location.href = biocacheUrl + "occurrences/searchForUID?q=" +
+              instanceUid + "&fq=" +
+              rank + ":" + name;
+    } else {
+      // clicked slice - drill down unless already at species
+      if (rank != "species") {
+        $('div#taxonChart').html('<img class="taxon-loading" alt="loading..." src="' + baseUrl + '/images/ala/ajax-loader.gif"/>');
+        loadTaxonChartByRank(instanceUid, dataTable.getValue(chart.getSelection()[0].row,0), dataTable.getTableProperty('rank'));
+      }
+      if ($('span#resetTaxonChart').html() == "") {
+        $('span#resetTaxonChart').html("reset to " + dataTable.getTableProperty('rank'));
+      }
+    }
+  });
+
+  //chart.draw(data, {width: 450, height: 300, title: 'My Daily Activities'});
+  chart.draw(dataTable, taxaChartOptions);
+
+  // show taxon caption
+  $('div#taxonChartCaption').css('visibility', 'visible');
+}
+/************************************************************\
+*
+\************************************************************/
+function resetTaxonChart() {
+  $('div#taxonChart').html('<img class="taxon-loading" alt="loading..." src="' + baseUrl + '/images/ala/ajax-loader.gif"/>');
+  loadTaxonChart(baseUrl, instanceUid, taxaThreshold);
+  $('span#resetTaxonChart').html("");
+}
+/*--(end)------------------ taxa breakdown charts ------------------------------*/
+
+
+/*------------------------ DECADE BREAKDOWN CHARTS -----------------------------*/
+var decadeChartOptions = {
+    width: 500,
+    height: 300,
+    chartArea: {left:50, top:30, width: "88%", height: "75%"},
+    title: "Additions by decade",
+    titleTextStyle: {color: "#555", fontName: 'Arial', fontSize: 15},
+    legend: "none"
+};
+/************************************************************\
+* Draw the chart
+\************************************************************/
+function drawDecadeChart(decadeData, uid, options) {
+  setChartOptions(decadeChartOptions, options);
+
+  var dataTable = new google.visualization.DataTable(decadeData,0.6);
+
+  if (dataTable.getNumberOfRows() > 0) {
+    var vis = new google.visualization.ColumnChart(document.getElementById('decadeChart'));
+
+    google.visualization.events.addListener(vis, 'select', function() {
+    var decade = dataTable.getValue(vis.getSelection()[0].row,0);
+    // TODO: handle 'earlier' label
+    if (decade != 'earlier' && decade.length > 3) {
+        decade = decade.substr(0,4);
+        var dateTo = addDecade(decade);
+        var dateRange = "occurrence_date:[" + decade + "-01-01T12:00:00Z%20TO%20" + dateTo + "-01-01T12:00:00Z]";
+        // eg. occurrence_date:[1990-01-01T12:00:00Z%20TO%202000-01-01T12:00:00Z]
+        document.location.href = biocacheUrl + "occurrences/searchForUID?q=" +
+            uid + "&fq=" + dateRange;
+      }
+    });
+    vis.draw(dataTable, decadeChartOptions);
+    // show caption
+    $('span.decadeChartCaption').css('visibility', 'visible');
+  } else {
+    // no data
+    $('div#decadeChart').css("display","none");
+  }
+}
+/************************************************************\
+* increment year by a decade, eg 1990 -> 2000
+\************************************************************/
+function addDecade(from) {
+  var num = parseInt(from);
+  return num + 10;
+}
+/*--(end)----------------- decade breakdown charts -----------------------------*/
+
+/*----------------------------- RECORDS MAP ------------------------------------*/
+/************************************************************\
+* Draw the map and legend
+\************************************************************/
+function mapRequestHandler(response) {
+  if (response.error != undefined) {
+    // set map url
+    $('#recordsMap').attr("src","${resource(dir:'images/map',file:'mapaus1_white-340.png')}");
+    // set legend url
+    $('#mapLegend').attr("src","${resource(dir:'images/map',file:'mapping-data-not-available.png')}");
+  } else {
+    // set map url
+    $('#recordsMap').attr("src",response.mapUrl);
+    // set legend url
+    $('#mapLegend').attr("src",response.legendUrl);
+  }
+}
+/*--(end)---------------------- records map ------------------------------------*/
+
+
+/*------------------------ GENERAL RECORDS & STATS -----------------------------*/
+/************************************************************\
+* Build phrase with num records and set to elements with id = numBiocacheRecords
+\************************************************************/
+function setNumbers(totalBiocacheRecords) {
+  var recordsClause = "";
+  switch (totalBiocacheRecords) {
+    case 0: recordsClause = "No records"; break;
+    case 1: recordsClause = "1 record"; break;
+    default: recordsClause = addCommas(totalBiocacheRecords) + " records";
+  }
+  $('#numBiocacheRecords').html(recordsClause);
+}
+/************************************************************\
+* Set options for a chart
+\************************************************************/
+function setChartOptions(globalOptions, options) {
+    // override with passed options
+    if (options != undefined) {
+        for (var key in options) {
+            if (options.hasOwnProperty(key)) {
+                globalOptions[key] = options[key];
+            }
+        }
+    }
+}
+/************************************************************\
+* Add commas to number strings
+\************************************************************/
+function addCommas(nStr)
+{
+    nStr += '';
+    x = nStr.split('.');
+    x1 = x[0];
+    x2 = x.length > 1 ? '.' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }
+    return x1 + x2;
+}
+/*--(end)----------------- general records & stats -----------------------------*/
+
+
+/************************************************************\
+*
+\************************************************************/
 function toggleRow( whichLayer ) {
   var elem, vis;
   if( document.getElementById ) // this is the way the standards work
@@ -13,6 +238,9 @@ function toggleRow( whichLayer ) {
   vis.display = (vis.display==''||vis.display=='table-row')?'none':'table-row';
 }
 
+/************************************************************\
+*
+\************************************************************/
 function toggleLayer( whichLayer ) {
   var elem, vis;
   if( document.getElementById ) // this is the way the standards work
@@ -28,6 +256,9 @@ function toggleLayer( whichLayer ) {
   vis.display = (vis.display==''||vis.display=='block')?'none':'block';
 }
 
+/************************************************************\
+*
+\************************************************************/
 function toggleHelp(obj) {
   node = findPrevious(obj.parentNode, 'td', 4);
   var div;
@@ -44,6 +275,9 @@ function toggleHelp(obj) {
   }
 }
 
+/************************************************************\
+*
+\************************************************************/
 function findPrevious(o, tag, limit){
   for(tag = tag.toLowerCase(); o = o.previousSibling;)
       if(o.tagName && o.tagName.toLowerCase() == tag)
@@ -53,6 +287,9 @@ function findPrevious(o, tag, limit){
   return null;
 }
 
+/************************************************************\
+*
+\************************************************************/
   function anySelected(idOfSelect, message) {
     var d = new Date();
     var time = d.getHours();
@@ -66,6 +303,9 @@ function findPrevious(o, tag, limit){
     }
   }
 
+/************************************************************\
+*
+\************************************************************/
 // opens email window for slightly obfuscated email addy
 var strEncodedAtSign = "(SPAM_MAIL@ALA.ORG.AU)";
 function sendEmail(strEncoded) {
@@ -80,6 +320,9 @@ function sendEmail(strEncoded) {
     return false;
 }
 
+/************************************************************\
+*
+\************************************************************/
 // opens email window and adds error info
 var strEncodedAtSign = "(SPAM_MAIL@ALA.ORG.AU)";
 function sendBugEmail(strEncoded, message) {
@@ -95,6 +338,9 @@ function sendBugEmail(strEncoded, message) {
     return false;
 }
 
+/************************************************************\
+*
+\************************************************************/
 function initializeLocationMap(canBeMapped,lat,lng) {
   var map;
   var marker;
@@ -128,6 +374,9 @@ function initializeLocationMap(canBeMapped,lat,lng) {
   }
 }
 
+/************************************************************\
+*
+\************************************************************/
 function contactCurator(email, firstName, uid, instUid, name) {
     var subject = "Request to review web pages presenting information about the " + name + ".";
     var content = "Dear " + firstName + ",\n\n";

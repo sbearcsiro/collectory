05aaa426d2330c39d5f6ee2237471364b84fd280
markew <mark.woolston@axon.com.au>
2011-01-12 02:26:33 +0000

    test for makeAbstract

--- test/unit/au/org/ala/collectory/ProviderGroupTests.groovy
+++ test/unit/au/org/ala/collectory/ProviderGroupTests.groovy
@@ -37,4 +37,17 @@ class ProviderGroupTests extends GrailsUnitTestCase {
         assertEquals 0, pg.getAttributionList().size()
     }
 
+    void testMakeAbstract() {
+        ProviderGroup pg = new Institution(guid: '237645', name: 'Bees')
+        pg.pubDescription = "Taxonomic research on Antarctic."
+        assertEquals 'Taxonomic research on Antarctic.', pg.makeAbstract(400)
+        pg.pubDescription = "Stuff.\nTaxonomic research on Antarctic.\n"
+        assertEquals 'Stuff. Taxonomic research on Antarctic.', pg.makeAbstract(400)
+        pg.pubDescription = "Taxonomic research on Antarctic.\nStuff."
+        assertEquals 'Taxonomic research on Antarctic. Stuff.', pg.makeAbstract(400)
+        pg.pubDescription = "Taxonomic research on Antarctic.\nStuff.\n"
+        assertEquals 'Taxonomic research on Antarctic. Stuff.', pg.makeAbstract(400)
+        pg.pubDescription = "Taxonomic research on Antarctic.\nStuff.\nMore stuff."
+        assertEquals 'Taxonomic research on Antarctic. Stuff.', pg.makeAbstract(400)
+    }
 }

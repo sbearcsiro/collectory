142a317e884bbd2b966e8d04561b908c77393562
markew <mark.woolston@axon.com.au>
2011-02-16 01:32:04 +0000

    add RESTful version of citation service add /ws path to uri for EML service refactor url mapping to use a common pattern for all entity data services add RESTful style service to return contacts for entities add mapping for web services catalogue

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -7,27 +7,49 @@ class UrlMappings {
 	  }
       "/lookup/inst/$inst/coll/$coll"(controller:'lookup',action:'collection')
       "/admin/export/$table" (controller:'admin',action:'export')
-      "/ws/collection/$uid?" (controller:'data') {
-          action = [GET:'getCollection', PUT:'saveCollection', DELETE:'delete', POST:'saveCollection']
-       }
-      "/ws/institution/$uid?" (controller:'data') {
-          action = [GET:'getInstitution', PUT:'saveInstitution', DELETE:'delete', POST:'saveInstitution']
-       }
-      "/ws/dataProvider/$uid?" (controller:'data') {
-          action = [GET:'getDataProvider', PUT:'saveDataProvider', DELETE:'delete', POST:'saveDataProvider']
-       }
-      "/ws/dataResource/$uid?" (controller:'data') {
-          action = [GET:'getDataResource', PUT:'saveDataResource', DELETE:'delete', POST:'saveDataResource']
-       }
-      "/ws/dataHub/$uid?" (controller:'data') {
-          action = [GET:'getDataHub', PUT:'saveDataHub', DELETE:'delete', POST:'saveDataHub']
-       }
+
+      // data services
+        "/ws/$entity/$uid?" (controller:'data') {
+          action = [HEAD: 'head', GET:'getEntity', PUT:'saveEntity', DELETE:'delete', POST:'saveEntity']
+          constraints {
+            entity(inList:['collection','institution','dataProvider','dataResource','dataHub'])
+          }
+        }
+
+      // entity contacts
+      "/ws/$entity/$uid/contact/$id?" {
+          controller = 'data'
+          action = 'contactForEntity'
+          constraints {
+              entity(inList:['collection','institution','dataProvider','dataResource','dataHub'])
+          }
+      }
+      // the next 5 rules should be able to be expressed as one rule in the same format as above
+      // BUT /ws/$entity/contact does not work for some reason
+      "/ws/collection/contact" { controller = 'data'; action = 'contactsForEntities'; entity = 'collection'}
+      "/ws/institution/contact" { controller = 'data'; action = 'contactsForEntities'; entity = 'institution'}
+      "/ws/dataProvider/contact" { controller = 'data'; action = 'contactsForEntities'; entity = 'dataProvider'}
+      "/ws/dataResource/contact" { controller = 'data'; action = 'contactsForEntities'; entity = 'dataResource'}
+      "/ws/dataHub/contact" { controller = 'data'; action = 'contactsForEntities'; entity = 'dataHub'}
+
+      // entities in a data hub
+      "/ws/dataHub/$uid/institutions" (controller: 'data', action: 'institutionsForDataHub')
+      "/ws/dataHub/$uid/collections" (controller: 'data', action: 'collectionsForDataHub')
+
+      // citations
+      "/ws/citations/$include" (controller:'lookup', action:'citations')
+
+      // eml
       "/eml/$id?" (controller:'data',action:'eml')
+      // preferred
+      "/ws/eml/$id?" (controller:'data',action:'eml')
+
       "/ws/collection/contacts/$uid?" (controller:'data',action:'contactsForCollections')
       "/ws/institution/contacts/$uid?" (controller:'data',action:'contactsForInstitutions')
       "/ws/dataProvider/contacts/$uid?" (controller:'data',action:'contactsForDataProviders')
       "/ws/dataResource/contacts/$uid?" (controller:'data',action:'contactsForDataResources')
       "/ws/dataHub/contacts/$uid?" (controller:'data',action:'contactsForDataHubs')
+      "/ws" (controller:'data',action:'catalogue')
       "/showConsumers/$id" (controller:'entity',action:'showConsumers')
       "/showProviders/$id" (controller:'entity',action:'showProviders')
       "/"(controller:'public', action:'map')

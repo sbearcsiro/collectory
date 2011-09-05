a6a4223351c538bc8cd580bf63a9109533e0a511
markew <mark.woolston@axon.com.au>
2011-09-05 05:23:24 +0000

    add support for tempDataResource

--- grails-app/services/au/org/ala/collectory/IdGeneratorService.groovy
+++ grails-app/services/au/org/ala/collectory/IdGeneratorService.groovy
@@ -15,7 +15,8 @@ class IdGeneratorService implements Serializable {
         dataProvider('dp'),
         dataResource('dr'),
         dataHub('dh'),
-        attribution('at')
+        attribution('at'),
+        tempDataResource('drt')
 
         public String prefix
         public IdType(prefix) {
@@ -46,15 +47,21 @@ class IdGeneratorService implements Serializable {
         return prefix + next
     }
 
+    /**
+     *
+     * @param uid
+     * @return
+     * @deprecated uids are all generated internally so this check is no longer required
+     */
     def checkValidId(String uid) {
         if (!uid || uid.size() < 3) {return false}
-        IdType type = IdType.lookup(uid[0..1])
+        IdType type = IdType.lookup(uid.startsWith('drt')?'drt':uid[0..1])
         if (!type) {return false}
         def id = type.getIndex()
         def sql = new Sql(dataSource)
         GroovyRowResult row = sql.firstRow("select next_id from sequence where id = ?",[id]) as GroovyRowResult
         long next = row.next_id
-        String uidValueStr = uid.substring(2)
+        String uidValueStr = uid.startsWith('drt')?'drt':uid[0..1]
         long uidValue
         try {
             uidValue = Long.parseLong(uidValueStr)
@@ -87,5 +94,8 @@ class IdGeneratorService implements Serializable {
     }
     def getNextAttributionId() {
         getNextId(IdType.attribution)
+    }
+    def getNextTempDataResource() {
+        getNextId(IdType.tempDataResource)
     }
 }

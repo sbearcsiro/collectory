a8430c1c57ceb7a9d0854ddc366de9245f42e9f7
markew <mark.woolston@axon.com.au>
2011-12-07 03:09:37 +0000

    isAuthorisedToEdit now uses the entity's isAuthorised method which supports authorisation by inheritance from a parent added methods to list authorised entities for a contact or a user

--- grails-app/services/au/org/ala/collectory/AuthService.groovy
+++ grails-app/services/au/org/ala/collectory/AuthService.groovy
@@ -28,14 +28,64 @@ class AuthService {
         } else {
             def email = RequestContextHolder.currentRequestAttributes()?.getUserPrincipal()?.attributes?.email
             if (email) {
-                Contact c = Contact.findByEmail(email)
-                if (c) {
-                    ContactFor cf = ContactFor.findByContactAndEntityUid(c, uid)
-                    return cf?.administrator
-                }
+                return ProviderGroup._get(uid)?.isAuthorised(email)
             }
         }
         return false
     }
 
+    /**
+     * Returns a list of entities that the specified user is authorised to edit.
+     *
+     * Note that more than one contact may correspond to the user's email address. In this
+     * case, the result is a union of the lists for each contact.
+     *
+     * @param email
+     * @return a map holding entities, a list of their uids and the latest modified date
+     */
+    def authorisedForUser(String email) {
+        def contacts = Contact.findAllByEmail(email)
+        switch (contacts.size()) {
+            case 0: return [sorted: [], keys: [], latestMod: null]
+            case 1: return authorisedForUser(contacts[0])
+            default:
+                def result = [sorted: [], keys: [], latestMod: null]
+                contacts.each {
+                    def oneResult = authorisedForUser(it)
+                    result.sorted += oneResult.sorted
+                    result.keys += oneResult.keys
+                    if (oneResult.latestMod > result.latestMod) { result.latestMod = oneResult.latestMod }
+                }
+                return result
+        }
+    }
+
+    /**
+     * Returns a list of entities that the specified contact is authorised to edit.
+     *
+     * @param contact
+     * @return a map holding entities, a list of their uids and the latest modified date
+     */
+    def authorisedForUser(Contact contact) {
+        // get list of contact relationships
+        def latestMod = null
+        def entities = [:]  // map by uid to remove duplicates
+        ContactFor.findAllByContact(contact).each {
+            if (it.administrator) {
+                def pg = ProviderGroup._get(it.entityUid)
+                if (pg) {
+                    entities.put it.entityUid, [uid: pg.uid, name: pg.name]
+                    if (it.dateLastModified > latestMod) { latestMod = it.dateLastModified }
+                }
+                // add children
+                pg.children().each { child ->
+                    def ch = ProviderGroup._get(child.uid)
+                    if (ch) {
+                        entities.put ch.uid, [uid: ch.uid, name: ch.name]
+                    }
+                }
+            }
+        }
+        return [sorted: entities.values().sort { it.name }, keys:entities.keySet().sort(), latestMod: latestMod]
+    }
 }

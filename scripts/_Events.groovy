dc7f5b36bf0f88abff7b1ac4a613e5459915423f
markew <mark.woolston@axon.com.au>
2013-05-22 03:48:50 +0000

    testing why _Events is not executed

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,13 +1,14 @@
 import org.codehaus.groovy.grails.commons.ConfigurationHolder
 
 eventWebXmlStart = {
-    if (!ConfigurationHolder.config.security.cas.bypass) {
+//    if (!ConfigurationHolder.config.security.cas.bypass) {
         def tmpWebXml = "${projectWorkDir}/web.xml.tmp"
+        println "projectWorkDir = ${projectWorkDir}"
         ant.replace(file: tmpWebXml, token: "@security.cas.serverName@", value: ConfigurationHolder.config.security.cas.serverName)
         println "Injecting CAS Security Configuration: serverName = ${ConfigurationHolder.config.security.cas.serverName}"
         ant.replace(file: tmpWebXml, token: "@security.cas.contextPath@", value: ConfigurationHolder.config.security.cas.contextPath)
         println "Injecting CAS Security Configuration: contextPath = ${ConfigurationHolder.config.security.cas.contextPath}"
         ant.replace(file: tmpWebXml, token: "/substitute-me", value: ConfigurationHolder.config.security.cas.urlPattern)
         println "Injecting CAS Security Configuration: url pattern = ${ConfigurationHolder.config.security.cas.urlPattern}"
-    }
+//    }
 }
